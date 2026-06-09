68937591bc6622e5079fd8dddbd8e460afd6d9df
brunohmdev <brunohm.dev@gmail.com>
2026-06-09 20:43:07 -0300

    add bcrypt para hashing de senhas

--- src/main/java/br/com/plashplanmycash/config/SecurityConfig.java
+++ src/main/java/br/com/plashplanmycash/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package br.com.plashplanmycash.config;
 
 import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         return http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configure(http))
@@ -19,5 +24,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/cadastro").permitAll()
                         .anyRequest().authenticated())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
